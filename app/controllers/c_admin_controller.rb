cb46705aa16c50bcf8142a603097237b52039e97
Munir Ahmad <munir@loop.pk>
2021-07-05 19:08:17 +0500

    Fixed minor redirect bug

--- app/controllers/c_admin_controller.rb
+++ app/controllers/c_admin_controller.rb
@@ -1,6 +1,6 @@
 class CAdminController < ApplicationController
  def upload
     Meme.create(title: params[:title], likes: params[:likes], meme: params[:file])
-    redirect_to "/memes"
+    redirect_to memes_path
  end
 end
