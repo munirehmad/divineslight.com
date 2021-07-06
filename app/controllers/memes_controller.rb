045937065519e8e043a648a27b31d693bac4962a
Uon Liaquat <antelopes06@gmail.com>
2021-07-06 22:34:26 +0500

    Added Memes Show Page

--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -4,4 +4,20 @@ class MemesController < ApplicationController
     @memes = Meme.all
   end
 
+  def show
+    @memes_path = memes_path
+    @memes_size = Meme.count
+    if params[:id].to_i > @memes_size 
+      @meme = Meme.last
+    else
+      @meme = Meme.find params[:id]
+    end
+  end
+
+  def like
+    meme = (Meme.find params[:id]).increment! :likes
+    redirect_to memes_path + '/' + meme.id.to_s
+  end
+
+
 end
