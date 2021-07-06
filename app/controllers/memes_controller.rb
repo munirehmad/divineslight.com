eb0efec6e61d94e2ad704f03ee5126a432e5f9ba
Uon Liaquat <antelopes06@gmail.com>
2021-07-06 15:07:15 +0500

    Resolved Queries on Comments

--- app/controllers/memes_controller.rb
+++ app/controllers/memes_controller.rb
@@ -5,20 +5,18 @@ class MemesController < ApplicationController
   end
 
   def show
+    @memes_path = memes_path
     @memes_size = Meme.count
-    if params[:id].to_i() > @memes_size 
+    if params[:id].to_i > @memes_size 
       @meme = Meme.last
     else
       @meme = Meme.find(params[:id])
-      puts
-      puts "likes: #{@meme.likes}"
-      puts
     end
   end
 
   def like
     meme = Meme.find(params[:id]).increment!(:likes)
-    redirect_to "/memes/#{meme.id}"
+    redirect_to memes_path + '/' + meme.id.to_s
   end
 
 
